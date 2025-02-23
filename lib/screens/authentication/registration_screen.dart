25229fffc2e00f508e53a59d8a0390bb69332cb8
viac5 <adronnyi.kollaider@gmail.com>
2025-02-23 17:54:41 +0300

    handling of incorrect input email

--- lib/screens/authentication/registration_screen.dart
+++ lib/screens/authentication/registration_screen.dart
@@ -15,17 +15,17 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   bool _isPasswordVisible = false;
   late String _verificationCode;
+  bool _isEmailValid = true;
 
   Future<void> _sendVerificationEmail(String email) async {
-
     final credentials = await _loadCredentials();
     String username = credentials['username']!;
     String password = credentials['password']!;
-
 
     final smtpServer = SmtpServer(
       'smtp.yandex.ru',
@@ -50,11 +50,23 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Verification email sent to $email')),
       );
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => VerificationScreen(
+          email: _emailController.text,
+          password: _passwordController.text,
+          sentCode: _verificationCode)),
+      );
+      
     } on MailerException catch (e) {
       print('Message not sent. \n' + e.toString());
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to send verification email')),
+        SnackBar(content: Text('Email введен неверно. Попробуйте еще раз.')),
       );
+      setState(() {
+        _isEmailValid = false;
+      });
       for (var p in e.problems) {
         print('Problem: ${p.code}: ${p.msg}');
       }
@@ -63,6 +75,9 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Unexpected error occurred')),
       );
+      setState(() {
+        _isEmailValid = false;
+      });
     }
   }
 
@@ -81,17 +96,17 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     };
   }
 
-
   Future<void> _registerWithEmailAndPassword() async {
-    await _sendVerificationEmail(_emailController.text);
+    // Сбрасываем состояние _isEmailValid перед каждой попыткой отправки сообщения
+    setState(() {
+      _isEmailValid = true;
+    });
 
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => VerificationScreen(
-        email: _emailController.text,
-        password: _passwordController.text,
-        sentCode: _verificationCode)),
-    );
+    if (_formKey.currentState!.validate()) {
+      await _sendVerificationEmail(_emailController.text);
+      // Trigger revalidation after email sending attempt
+      _formKey.currentState!.validate();
+    }
   }
 
   @override
@@ -116,92 +131,107 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Sign Up',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Serif',  // Замените на шрифт по вашему выбору
-                  ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  width: 300,  // Установите нужную ширину
-                  child: TextField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      hintText: 'name@example.com',
-                      border: OutlineInputBorder(),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Serif',  // Замените на шрифт по вашему выбору
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                  width: 300,  // Установите нужную ширину
-                  child: TextField(
-                    controller: _passwordController,
-                    obscureText: !_isPasswordVisible,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      //hintText: '••••••••',
-                      border: OutlineInputBorder(),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                  SizedBox(height: 20),
+                  Container(
+                    width: 300,  // Установите нужную ширину
+                    child: TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'name@example.com',
+                        border: OutlineInputBorder(),
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red, width: 2.0),
                         ),
-                        onPressed: () {
-                          setState(() {
-                            _isPasswordVisible = !_isPasswordVisible;
-                          });
-                        },
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red, width: 2.0),
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _registerWithEmailAndPassword,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,  // Установите нужный цвет кнопки
-                    minimumSize: Size(300, 50),  // Установите нужные размеры
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                  child: Text('Verify your email', style: TextStyle(color: Colors.black)),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 140,  // Ширина кнопки Forgot Password
-                      child: TextButton(
-                        onPressed: () {
-                          // Обработка восстановления пароля
-                        },
-                        child: Text('Forgot Password?', style: TextStyle(color: Colors.black)),
-                      ),
-                    ),
-                    Container(
-                      width: 140,  // Ширина кнопки Sign In
-                      child: TextButton(
-                        onPressed: () {
-                         Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => LoginScreen()),
-                        );
+                      validator: (value) {
+                        if (!_isEmailValid) {
+                          return 'Invalid email address';
+                        }
+                        return null;
                       },
-                      child: Text('Sign In', style: TextStyle(color: Colors.black)),
-                      )
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: 300,  // Установите нужную ширину
+                    child: TextFormField(
+                      controller: _passwordController,
+                      obscureText: !_isPasswordVisible,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        //hintText: '••••••••',
+                        border: OutlineInputBorder(),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _isPasswordVisible = !_isPasswordVisible;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _registerWithEmailAndPassword,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,  // Установите нужный цвет кнопки
+                      minimumSize: Size(300, 50),  // Установите нужные размеры
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Text('Verify your email', style: TextStyle(color: Colors.black)),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 140,  // Ширина кнопки Forgot Password
+                        child: TextButton(
+                          onPressed: () {
+                            // Обработка восстановления пароля
+                          },
+                          child: Text('Forgot Password?', style: TextStyle(color: Colors.black)),
+                        ),
+                      ),
+                      Container(
+                        width: 140,  // Ширина кнопки Sign In
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => LoginScreen()),
+                            );
+                          },
+                          child: Text('Sign In', style: TextStyle(color: Colors.black)),
+                        )
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
